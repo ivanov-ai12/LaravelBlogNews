f13b8d2fab2f87222ddb10a6436381a728658034
ivanov-ai12 <ivanov-ai12@list.ru>
2019-08-26 20:46:08 +0300

    add database
    add database

--- database/migrations/2019_08_26_173852_update_post_tablee.php
+++ database/migrations/2019_08_26_173852_update_post_tablee.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdatePostTable extends Migration
+class UpdatePostTablee extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class UpdatePostTable extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table){
-           $table->string('alias');
-        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('alias');
+         });
     }
 
     /**
@@ -25,8 +25,6 @@ class UpdatePostTable extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table){
-            $table->dropColumn('alias');
-        });
+        $table->dropColumn('alias');
     }
 }
